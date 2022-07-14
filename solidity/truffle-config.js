c997b1ac1f2272439d15959910dd9ddffde49e95
l3lackMegas ༼ つ ◕_◕ ༽つ <jara7897@gmail.com>
2022-07-14 14:36:50 +0700

    chore: free private key, lmao

--- solidity/truffle-config.js
+++ solidity/truffle-config.js
@@ -1,5 +1,6 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const provider = new HDWalletProvider({
+     // Here is your free crypto wallet, don't open issue about this shit.
      privateKeys: ['ee678a5087c3b897a79dba934467b428125e86235b3807a1f63e6c54ac8d633f'],
      providerOrUrl: 'https://data-seed-prebsc-1-s1.binance.org:8545/'
 });
